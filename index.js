3b4be0e29af51a474c8ffbd36388b6d426f81144
binbin <390623320@qq.com>
2020-09-20 23:31:26 +0800

    init

---
+++ index.js
@@ -0,0 +1,15 @@
+var express = require("express");
+var app = express();
+var logger = require('morgan');
+var router = require('./router/router');
+var TimeStamepLogger = function(req,res,next){
+    console.log("Time:" + new Date().toLocaleString())
+    next();
+}
+app.listen("8080",()=>{
+    console.log("NODE服务器启动:localhost:8080")
+});
+app.use(TimeStamepLogger)
+app.use(router);
+app.use(logger('dev'));
+app.use('/static', express.static('src'));
