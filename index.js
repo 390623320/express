6c005b3d6c2798474e21656dd3858535696921b2
binbin <390623320@qq.com>
2020-09-20 23:45:30 +0800

    uodata

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ var TimeStamepLogger = function(req,res,next){
     console.log("Time:" + new Date().toLocaleString())
     next();
 }
-app.listen("8080",()=>{
+app.listen("8081",()=>{
     console.log("NODE服务器启动:localhost:8080")
 });
 app.use(TimeStamepLogger)
 app.use(router);
 app.use(logger('dev'));
-app.use('/static', express.static('src'));
+app.use('/static',express.static(__dirname + '/src'));
