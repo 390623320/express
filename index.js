966fdd222d759ba71c5fe0c4f4f25d0829236e10
binbin <390623320@qq.com>
2020-09-28 14:25:27 +0800

    commit

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require("express");
 var app = express();
 var logger = require('morgan');
 var router = require('./router/router');
+var bodyParser = require('body-parser')
 var TimeStamepLogger = function(req,res,next){
     console.log("Time:" + new Date().toLocaleString())
     next();
@@ -9,7 +10,11 @@ var TimeStamepLogger = function(req,res,next){
 app.listen("8081",()=>{
     console.log("NODE服务器启动:localhost:8080")
 });
-app.use(TimeStamepLogger)
-app.use(router);
-app.use(logger('dev'));
+app.use(bodyParser.urlencoded({extended:false})); //解析Post-body
+app.use(bodyParser.json());//将body转换为JSON格式
 app.use('/static',express.static(__dirname + '/src'));
+app.use(router);
+app.use(TimeStamepLogger)
+app.use(logger('dev'));
+
+
--- router/router.js
+++ router/router.js
@@ -3,5 +3,13 @@ var router = express.Router();
 router.get('/',(req,res)=>{
     res.send("ni");
 });
+router.get('/form',(req,res)=>{  //GET:query
+    console.log(req.query)
+    res.send("ni");
+});
+router.post('/form',(req,res)=>{ //POST:body
+    console.log(req.body)
+    res.send("ni");
+});
 
 module.exports = router;
